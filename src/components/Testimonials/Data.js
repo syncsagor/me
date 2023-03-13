f0eb05aa703890d19d35e5a48f68a41866f84cc2
Mohammad Sagor Ali <sagorbbr1@gmail.com>
2023-03-13 23:18:44 +0600

    more testimonial photo added

--- src/components/Testimonials/Data.js
+++ src/components/Testimonials/Data.js
@@ -1,6 +1,9 @@
 import Image1 from '../../assets/testimonial1.png';
 import Image2 from '../../assets/testimonial2.png';
 import Image3 from '../../assets/testimonial3.png';
+import Image4 from '../../assets/testimonial4.jpg';
+import Image5 from '../../assets/testimonial5.jpg';
+import Image6 from '../../assets/testimonial6.jpg';
 
 export const Data = [
     {
@@ -23,5 +26,26 @@ export const Data = [
       title: "Sara Cill",
       description:
         "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+    {
+      id: 4,
+      image: Image4,
+      title: "Nick Smith",
+      description:
+        "I'm very happy for building my website. It is so much responsive and user-friendly. Highly recommended this guy!",
+    },
+    {
+      id: 5,
+      image: Image5,
+      title: "David Michael",
+      description:
+        "Good job! That was very exiting Project and i'm satisfied with this, Thanks.",
+    },
+    {
+      id: 6,
+      image: Image6,
+      title: "John Mickey",
+      description:
+        "Amazing!! Hope to work with you again.",
     },
   ];
